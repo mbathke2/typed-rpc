de289bf08209206346754ce68fbcea1c3817dcea
Michael Bathke <mb@cryptostruct.com>
2025-04-29 08:24:01 +0200

    fix: passing the paramsType through the sendRequest method

--- src/client.ts
+++ src/client.ts
@@ -176,7 +176,7 @@ export function rpcClient<T extends object>(options: RpcClientOptions) {
       if (prop === "toJSON") return;
       return (...args: any) => {
         const ac = new AbortController();
-        const promise = sendRequest(prop.toString(), args, ac.signal);
+        const promise = sendRequest(prop.toString(), args, ac.signal, paramsType);
         abortControllers.set(promise, ac);
         promise
           .finally(() => {
